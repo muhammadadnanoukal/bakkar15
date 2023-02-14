94e9d6d4e34a78213b606429d1fbf7a7bd0042b6
muhammadadnanoukal <101559768+muhammadadnanoukal@users.noreply.github.com>
2023-02-14 13:31:48 +0300

    Add files via upload

---
+++ ALTANMYA_Approvals_Extension/models/approval_category_inherit.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+from odoo import api
+from odoo.exceptions import ValidationError
+
+
+class ApprovalCategory(models.Model):
+    _inherit = 'approval.category'
+
+    approver_sequence = fields.Boolean('Approvers Sequence?')
+    line_manager_approval = fields.Selection([('approver', 'Is Approver'), ('required', 'Is Required Approver')],
+                                             string="Employee's Line Manager")
+    notify_even_when_refused = fields.Boolean(string='Notify Approvers Even If Refused')
+
+    @api.constrains('approver_sequence')
+    def _constrains_approver_sequence(self):
+        if any(a.approver_sequence and not a.approval_minimum for a in self):
+            raise ValidationError('')
